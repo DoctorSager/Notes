e13981947fd1f80abd62503fc373f6b4b25513af
DoctorSager <doctorsager163@gmail.com>
2023-10-19 16:27:16 +0300

    [Notes]

---
+++ NoteTaking/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace NoteTaking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bttnNew = new System.Windows.Forms.Button();
+            this.bttnSave = new System.Windows.Forms.Button();
+            this.bttnLoad = new System.Windows.Forms.Button();
+            this.bttnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(205, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Название/Заголовок";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(397, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Текст ";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtTitle.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtTitle.Location = new System.Drawing.Point(199, 56);
+            this.txtTitle.Multiline = true;
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(169, 34);
+            this.txtTitle.TabIndex = 2;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtMessage.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtMessage.Location = new System.Drawing.Point(401, 56);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(377, 174);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(199, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(169, 123);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // bttnNew
+            // 
+            this.bttnNew.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bttnNew.Location = new System.Drawing.Point(31, 28);
+            this.bttnNew.Name = "bttnNew";
+            this.bttnNew.Size = new System.Drawing.Size(134, 28);
+            this.bttnNew.TabIndex = 5;
+            this.bttnNew.Text = "Создать";
+            this.bttnNew.UseVisualStyleBackColor = true;
+            this.bttnNew.Click += new System.EventHandler(this.bttnNew_Click);
+            // 
+            // bttnSave
+            // 
+            this.bttnSave.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bttnSave.Location = new System.Drawing.Point(31, 86);
+            this.bttnSave.Name = "bttnSave";
+            this.bttnSave.Size = new System.Drawing.Size(134, 28);
+            this.bttnSave.TabIndex = 6;
+            this.bttnSave.Text = "Сохранить";
+            this.bttnSave.UseVisualStyleBackColor = true;
+            this.bttnSave.Click += new System.EventHandler(this.bttnSave_Click);
+            // 
+            // bttnLoad
+            // 
+            this.bttnLoad.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bttnLoad.Location = new System.Drawing.Point(31, 144);
+            this.bttnLoad.Name = "bttnLoad";
+            this.bttnLoad.Size = new System.Drawing.Size(134, 28);
+            this.bttnLoad.TabIndex = 7;
+            this.bttnLoad.Text = "Загрузить";
+            this.bttnLoad.UseVisualStyleBackColor = true;
+            this.bttnLoad.Click += new System.EventHandler(this.bttnLoad_Click);
+            // 
+            // bttnDelete
+            // 
+            this.bttnDelete.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bttnDelete.Location = new System.Drawing.Point(31, 202);
+            this.bttnDelete.Name = "bttnDelete";
+            this.bttnDelete.Size = new System.Drawing.Size(134, 28);
+            this.bttnDelete.TabIndex = 8;
+            this.bttnDelete.Text = "Удалить";
+            this.bttnDelete.UseVisualStyleBackColor = true;
+            this.bttnDelete.Click += new System.EventHandler(this.bttnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 254);
+            this.Controls.Add(this.bttnDelete);
+            this.Controls.Add(this.bttnLoad);
+            this.Controls.Add(this.bttnSave);
+            this.Controls.Add(this.bttnNew);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bttnNew;
+        private System.Windows.Forms.Button bttnSave;
+        private System.Windows.Forms.Button bttnLoad;
+        private System.Windows.Forms.Button bttnDelete;
+    }
+}
+
